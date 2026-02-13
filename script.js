ad5171bb1ecd1daaa55e7db11a0941ee400c0a4b
Kapil Dagar <116935052+happydagar6@users.noreply.github.com>
2026-02-13 12:19:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	for(let ch of str) {
+		if(str.indexOf(ch) === str.lastIndexOf(ch)){
+			return ch;
+		}
+	}
+	return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
